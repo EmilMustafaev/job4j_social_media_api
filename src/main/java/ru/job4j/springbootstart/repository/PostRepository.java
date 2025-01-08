c9d7f0e321b857f88b2490393f178a350c85ca70
EmilMustafaev <emilmustafaev891@gmail.com>
2025-01-08 13:07:03 +0300

    Именованные запросы [#505063]

--- src/main/java/ru/job4j/springbootstart/repository/PostRepository.java
+++ src/main/java/ru/job4j/springbootstart/repository/PostRepository.java
@@ -1,7 +1,18 @@
 package ru.job4j.springbootstart.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import ru.job4j.springbootstart.model.Post;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface PostRepository extends CrudRepository<Post, Long> {
+
+    List<Post> findByUserId(Long userId);
+
+    List<Post> findByCreatedAtBetween(LocalDateTime startDate, LocalDateTime endDate);
+
+    Page<Post> findAllByOrderByCreatedAtDesc(Pageable pageable);
 }
