7dacb9990afd092250c45155551d6a78e6017eab
EmilMustafaev <emilmustafaev891@gmail.com>
2025-01-08 17:41:50 +0300

    Spring Data JPA Test [#505130 #527565]

--- src/test/java/ru/job4j/springbootstart/SpringbootstartApplicationTests.java
+++ src/test/java/ru/job4j/springbootstart/SpringbootstartApplicationTests.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
-
-@Disabled
 @SpringBootTest
 @TestPropertySource("classpath:application-test.properties")
 class SpringbootstartApplicationTests {
